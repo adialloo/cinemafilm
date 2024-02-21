ab12fbcd3e70fcf8e712eba0eb96d2011d4e3262
Aly DIALLO <aly.diallo@mines-ales.org>
2024-02-21 17:22:01 +0100

    suppression test

--- tests/test_api.py
+++
@@ -1,9 +0,0 @@
-# tests/test_api.py
-
-from app import get_movies
-
-
-def test_get_movies():
-    url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=46f3de7d1fd8790343c98572d954a595'
-    movies = get_movies(url)
-    assert movies is not None
